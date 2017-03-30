ddea6adbbc12d23863af0156915bc69a4cff68be
Ilya <ilyaagent765@gmail.com>
2017-03-30 22:43:00 +0400

    My first servlet

---
+++ MainServlet.java
@@ -0,0 +1,33 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+/**
+ * Created by USER on 28.03.2017.
+ */
+import javax.servlet.ServletException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet("/s")
+public class MainServlet extends HttpServlet {
+    public static void main(String[] args) {
+
+//        public void doGet(HttpServletRequest request,
+//                HttpServletResponse response)
+//throws ServletException, IOException {
+//            response.setContentType("text/html"); // Установка типа документа
+//            PrintWriter out = response.getWriter();
+//            out.println("<html> <body> Доброе Утро !!! </body></html>");
+//            out.close();
+
+
+
+
+        }
+    }
+
+
+
